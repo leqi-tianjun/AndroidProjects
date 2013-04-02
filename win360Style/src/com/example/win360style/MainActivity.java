f3d29ef3dcaec205944a437abd18e61546291ffb
leqi <qiulele830000@hotmail.com>
2013-04-03 00:48:09 +0800

    Added right menu. Still have bugs need to fix

--- win360Style/src/com/example/win360style/MainActivity.java
+++ win360Style/src/com/example/win360style/MainActivity.java
@@ -1,20 +1,42 @@
 package com.example.win360style;
 
 import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.LinearLayout;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	private Context context=this;
 	private ViewPager myViewPager;
+	private FrameLayout mSettingFrameLayout;
+	private FrameLayout mMainFrameLayout;
+
+	private boolean mSlided = false;
+	private ImageButton mSettingBtn;
+
+	private final static int TRANSLATE_ANIMATION_WIDTH = 150;
+	private final static int ANIMATION_DURATION_FAST = 450;
+	private final static int ANIMATION_DURATION_SLOW = 350;
+	private final static int MOVE_DISTANCE = 50;
+	
+	// 屏幕宽度
+	private int mWidth;
+	private float mPositionX;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -65,5 +87,155 @@ public class MainActivity extends Activity {
 			 }
 		};
 		myViewPager.setAdapter(adapter);
+
+		mWidth = getResources().getDisplayMetrics().widthPixels;
+		mSettingFrameLayout = (FrameLayout) findViewById(R.id.setting);
+		mMainFrameLayout = (FrameLayout) findViewById(R.id.main);
+//		mMainFrameLayout.setOnTouchListener(mOnTouchListener);
+//		slideIn();
+				
+		mSettingBtn = (ImageButton) findViewById(R.id.setting_btn);
+		mSettingBtn.setOnClickListener(mOnClickListener);
 	}
+	
+	// 点击按钮
+		private OnClickListener mOnClickListener = new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				switch (v.getId()) {
+					case R.id.setting_btn :
+						if (mSlided) {
+							slideIn();
+						} else {
+							slideOut();
+						}
+						break;
+				}
+			}
+		};
+		
+		// 滑动
+		private OnTouchListener mOnTouchListener = new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				if (v.getId() == R.id.main) {
+					int action = event.getAction();
+					switch (action) {
+						case MotionEvent.ACTION_DOWN :
+							mPositionX = event.getX();
+							break;
+						case MotionEvent.ACTION_MOVE :
+							final float currentX = event.getX();
+							// 向左边滑动
+							if (currentX - mPositionX <= -MOVE_DISTANCE && !mSlided) {
+								slideOut();
+							} else if (currentX - mPositionX >= MOVE_DISTANCE && mSlided) {
+								slideIn();
+							}
+							break;
+					}
+					return true;
+				} 
+				return false;
+			}
+		};
+
+		/**
+		 * 滑出侧边栏
+		 */
+		private void slideOut() {
+			TranslateAnimation translate = new TranslateAnimation(mWidth,
+					mWidth-TRANSLATE_ANIMATION_WIDTH, 0, 0);
+			translate.setDuration(ANIMATION_DURATION_SLOW);
+			translate.setFillAfter(true);
+			mSettingFrameLayout.startAnimation(translate);
+			mSettingFrameLayout.getAnimation().setAnimationListener(
+					new Animation.AnimationListener() {
+						
+						@Override
+						public void onAnimationStart(Animation anim) {
+						}
+
+						@Override
+						public void onAnimationRepeat(Animation animation) {
+
+						}
+
+						@Override
+						public void onAnimationEnd(Animation anima) {
+							TranslateAnimation animation = new TranslateAnimation(
+									0, -TRANSLATE_ANIMATION_WIDTH, 0, 0);
+							animation.setDuration(ANIMATION_DURATION_FAST);
+							animation.setFillAfter(true);
+							mMainFrameLayout.startAnimation(animation);
+							mMainFrameLayout.getAnimation().setAnimationListener(new Animation.AnimationListener() {
+								
+								@Override
+								public void onAnimationStart(Animation animation) {
+									// TODO Auto-generated method stub
+									
+								}
+								
+								@Override
+								public void onAnimationRepeat(Animation animation) {
+									// TODO Auto-generated method stub
+									
+								}
+								
+								@Override
+								public void onAnimationEnd(Animation animation) {
+									mSettingFrameLayout.setVisibility(View.VISIBLE);
+									
+								}
+							});
+							mSlided = true;
+//							mSettingFrameLayout.setVisibility(View.VISIBLE);
+						}
+					});
+		}
+
+		/**
+		 * 滑进侧边栏
+		 */
+		private void slideIn() {
+			TranslateAnimation translate = new TranslateAnimation(mWidth-TRANSLATE_ANIMATION_WIDTH, mWidth, 0, 0);
+			translate.setDuration(ANIMATION_DURATION_FAST);
+			// 动画完成时停在结束位置
+			translate.setFillAfter(true);
+			mSettingFrameLayout.startAnimation(translate);
+			mSettingFrameLayout.getAnimation().setAnimationListener(
+					new Animation.AnimationListener() {
+
+						@Override
+						public void onAnimationStart(Animation animation) {
+							mSettingFrameLayout.setVisibility(View.GONE);
+							TranslateAnimation mainAnimation = new TranslateAnimation(
+									-TRANSLATE_ANIMATION_WIDTH, 0, 0, 0);
+							mainAnimation.setDuration(ANIMATION_DURATION_SLOW);
+							mainAnimation.setFillAfter(true);
+							mMainFrameLayout.startAnimation(mainAnimation);
+							
+						}
+
+						@Override
+						public void onAnimationRepeat(Animation animation) {
+
+						}
+
+						@Override
+						public void onAnimationEnd(Animation animation) {
+							mSlided = false;
+						}
+					});
+
+		}
+		
+		@Override
+		public boolean onKeyDown(int keyCode, KeyEvent event) {
+			if (keyCode == KeyEvent.KEYCODE_BACK && mSlided) {
+				slideIn();
+				return true;
+			}
+			return super.onKeyDown(keyCode, event);
+		}
 }
